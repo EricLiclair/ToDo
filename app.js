617ca844f8988fb5d45f5ee160a7d50d148c6ff2
Shubham Kushwaha <eric.liclair@gmail.com>
2020-10-26 21:03:00 +0530

    connected to localStorage

--- app.js
+++ app.js
@@ -24,23 +24,65 @@ fetch('./trial_data.json')
   })
 })
 
-addButton.addEventListener('click', function() {
-  if (placeholder.value == "") {
+//function to add the tasks previously stored in the local storage
+function addFromLocalStorage (value, taskStatus) {
+  let task = document.createElement('li');
+  task.setAttribute('id', 'tasks');
+  task.innerText = value;
+  task.style.cursor = 'pointer';
+  if (taskStatus == "Done") {
+    task.style.textDecorationLine="line-through";
+  }
+  taskView.appendChild(task);
+  task.addEventListener('click', function() {
+    task.style.textDecorationLine="line-through";
+    localStorage.setItem(value, "Done");
+  })
+
+  task.addEventListener('dblclick', function() {
+    localStorage.removeItem(value);
+    taskView.removeChild(task);
+  })
+
+}
+
+
+for (let i = 0; i < localStorage.length; i++) {
+  document.getElementById('task-viewer').style.display="flex";
+  let value = localStorage.key(i);
+  let taskStatus = localStorage.getItem(value);
+  addFromLocalStorage(value, taskStatus);
+}
+
+
+
+//function to add a new task from the input
+function addNewToDo() {
+  let value = placeholder.value;
+  if (value == "") {
     return ;
   }
   document.getElementById('task-viewer').style.display="flex";
-  var task = document.createElement('li');
+  let task = document.createElement('li');
   task.setAttribute('id', 'tasks');
-  task.innerText = placeholder.value;
+  task.innerText = value;
+  localStorage.setItem(value, "To Do");
   taskView.appendChild(task);
   placeholder.value = null ;
   task.style.cursor = 'pointer';
   task.addEventListener('click', function() {
     task.style.textDecorationLine="line-through";
+    localStorage.setItem(value, "Done");
+
   })
 
   task.addEventListener('dblclick', function() {
+    localStorage.removeItem(value);
     taskView.removeChild(task);
   })
   
-})
+}
+
+addButton.addEventListener('click', function() {
+  addNewToDo();  
+ })
